6063954826d7fe976f44b656deb6385c9c9cfb35
ChernovaNatalya <natali-4ch@yandex.ru>
2025-03-23 17:28:02 +0300

    first try1

--- include/circle.h
+++ include/circle.h
@@ -1,24 +1,26 @@
 // Copyright 2022 UNN-CS
+#ifndef INCLUDE_CIRCLE_H_
+#define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
 class Circle {
 public:
-	explicit Circle(double rad = 0);
+    explicit Circle(double rad = 0);
 
-	void setRadius(double rad);
-	void setFerence(double fer);
-	void setArea(double ar);
+    void setRadius(double rad);
+    void setFerence(double fer);
+    void setArea(double ar);
 
-	double getRadius() const;
-	double getFerence() const;
-	double getArea() const;
+    double getRadius() const;
+    double getFerence() const;
+    double getArea() const;
 
 private:
-	double radius;
-	double ference;
-	double area;
+    double radius;
+    double ference;
+    double area;
 
 };
 
-
+#endif  // INCLUDE_CIRCLE_H_
 
